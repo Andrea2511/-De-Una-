35c7c492db2d8c10974135080a35d3c1e822ca91
Andrea2511 <andreittha4507@gmail.com>
2023-11-22 20:15:20 -0500

    menu semanal, admin

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -10,7 +10,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const productForm = document.getElementById("producto-form");
     const insumoForm = document.getElementById("insumos-form");
     const inventory = document.getElementById("inventory");
-    let addProductBtn;
+    const menu = document.getElementById("menu");
+    const filas = document.getElementById('tablaInsumos').getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+    const filasComida = document.getElementById('tablaComidas').getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+    const filasPorPagina = 8;
+    let paginaActual = 1;
 
     menuBtn.addEventListener('click', () => {
         sideMenu.style.display = 'block';
@@ -46,6 +50,7 @@ document.addEventListener('DOMContentLoaded', function() {
             hideElement(productForm);
             hideElement(insumoForm);
             hideElement(inventory);
+            hideElement(menu);
             break;
         case "pqrs":
             document.querySelector(".pqrs").classList.add("active");
@@ -54,6 +59,7 @@ document.addEventListener('DOMContentLoaded', function() {
             hideElement(inventory);
             hideElement(productForm);
             hideElement(insumoForm);
+            hideElement(menu);
             break;
         case "inventario":
             document.querySelector(".inventario").classList.add("active");
@@ -62,19 +68,31 @@ document.addEventListener('DOMContentLoaded', function() {
             hideElement(productForm);
             hideElement(insumoForm);
             showElement(inventory);
+            hideElement(menu);
             break;
         case "menu":
             document.querySelector(".menu").classList.add("active");
             hideElement(main);
-            hideElement(right);
+            showElement(right);
+            showElement(menu);
             hideElement(inventory);
             hideElement(productForm);
             hideElement(insumoForm);
+            break;
+        case "guardarPromociones":
+            document.querySelector(".inventario").classList.add("active");
+            hideElement(main);
+            showElement(right);
+            hideElement(menu);
+            hideElement(productForm);
+            hideElement(insumoForm);
+            hideElement(inventory);
             break;
         case "guardarProducto":
             document.querySelector(".addP").classList.add("active");
             hideElement(main);
             showElement(right);
+            hideElement(menu);
             hideElement(inventory);
             showElement(productForm);
             hideElement(insumoForm);
@@ -83,6 +101,7 @@ document.addEventListener('DOMContentLoaded', function() {
             document.querySelector(".addI").classList.add("active");
             hideElement(main);
             showElement(right);
+            hideElement(menu);
             hideElement(inventory);
             hideElement(productForm);
             showElement(insumoForm);
@@ -90,10 +109,75 @@ document.addEventListener('DOMContentLoaded', function() {
         default:
             hideElement(main);
             hideElement(right);
+            hideElement(menu);
             hideElement(inventory);
             hideElement(productForm);
             hideElement(insumoForm);
             break;
     }
+
+    // Función para cambiar la página
+    function cambiarPagina(direccion) {
+        // Calcula la nueva página
+        paginaActual += direccion;
+
+        // Actualiza la visibilidad de las filas
+        actualizarVisibilidadFilas();
+
+        // Actualiza el número de página en el span
+        document.getElementById('paginaActual').innerText = paginaActual;
+    }
+
+    // Función para actualizar la visibilidad de las filas
+    function actualizarVisibilidadFilas() {
+        for (let i = 0; i < filas.length; i++) {
+            if (i >= (paginaActual - 1) * filasPorPagina && i < paginaActual * filasPorPagina) {
+                filas[i].classList.add('visible');
+            } else {
+                filas[i].classList.remove('visible');
+            }
+        }
+
+        // Actualiza el estado de los botones de paginación
+        document.getElementById('anteriorBtn').disabled = paginaActual === 1;
+        document.getElementById('siguienteBtn').disabled = paginaActual === Math.ceil(filas.length / filasPorPagina);
+    }
+
+    document.getElementById('anteriorBtn').addEventListener('click', () => cambiarPagina(-1));
+    document.getElementById('siguienteBtn').addEventListener('click', () => cambiarPagina(1));
+    // Inicializa la visibilidad de las filas
+    actualizarVisibilidadFilas();
+
+    function cambiarPaginaComida(direccion) {
+        // Calcula la nueva página
+        paginaActual += direccion;
+
+        // Actualiza la visibilidad de las filas
+        actualizarVisibilidadFilasComida();
+
+        // Actualiza el número de página en el span
+        document.getElementById('paginaActualComida').innerText = paginaActual;
+    }
+
+// Función para actualizar la visibilidad de las filas de comida
+    function actualizarVisibilidadFilasComida() {
+        for (let i = 0; i < filasComida.length; i++) {
+            if (i >= (paginaActual - 1) * filasPorPagina && i < paginaActual * filasPorPagina) {
+                filasComida[i].classList.add('visible');
+            } else {
+                filasComida[i].classList.remove('visible');
+            }
+        }
+
+        // Actualiza el estado de los botones de paginación
+        document.getElementById('anteriorBtnComida').disabled = paginaActualComida === 1;
+        document.getElementById('siguienteBtnComida').disabled = paginaActualComida === Math.ceil(filasComida.length / filasPorPaginaComida);
+    }
+
+    document.getElementById('anteriorBtnComida').addEventListener('click', () => cambiarPaginaComida(-1));
+    document.getElementById('siguienteBtnComida').addEventListener('click', () => cambiarPaginaComida(1));
+
+// Inicializa la visibilidad de las filas de comida
+    actualizarVisibilidadFilasComida();
 
 });
